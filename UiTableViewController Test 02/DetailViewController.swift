82a768a6b0065d7969394ad33c58df8670d5b671
D7703_16 <d7703_16@D7703-16ui-iMac.local>
2018-05-30 15:56:40 +0900

    Initial Commit

---
+++ UiTableViewController Test 02/DetailViewController.swift
@@ -0,0 +1,77 @@
+
+import UIKit
+
+class DetailViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var cellImageView: UIImageView!
+    @IBOutlet weak var detailTableView: UITableView!
+    
+    var cellImage = ""
+    var telNum: String = ""
+    var locationName: String = ""
+    var menu: String = ""
+    //김준범
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //delegate connection
+        detailTableView.delegate = self
+        detailTableView.dataSource = self
+
+        cellImageView.image = UIImage(named: cellImage)
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "detailCell", for: indexPath)
+        
+        
+        //        if indexPath.row == 0 {
+        //            cell.textLabel?.text = "주소 :" + loca2
+        //            return cell
+        //        } else if indexPath.row == 1 {
+        //            cell.textLabel?.text = "전화번호 :" + telNum2
+        //            return cell
+        //        } else {
+        //            cell.textLabel?.text = "메뉴 :" + menu
+        //            return cell
+        //        }
+        //if문 사용
+        
+        
+        switch indexPath.row  {
+        case 0:
+            cell.textLabel?.text = " 주소 : " + locationName
+            return cell
+         case 1:
+            cell.textLabel?.text = " 전화번호 : " + telNum
+            return cell
+         default:
+            cell.textLabel?.text = " 메뉴 : " + menu
+            return cell
+        }
+    }
+   
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
